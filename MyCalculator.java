90e0b524d0f8aa0751bd1ef5db2013fe62bc4663
patriciajune <tciaa01@gmail.com>
2015-02-28 19:27:51 +0800

    Update MyCalculator.java

--- MyCalculator.java
+++ MyCalculator.java
@@ -35,29 +35,60 @@ public class MyCalculator {
 	}
 	
 	// n! of an integer
-	public int nfactorial(int n){
-		int factorial = 1;
+	public static int nfactorial(int n){	
+		int factorial = 1;	
 		
-		for(int i=1;i<n;i++)
+		if(n<2){	//I added this portion
+			return factorial;
+		}
+		
+		for(int i=2;i<=n;i++)	//I edited this line; start with 2 bec 1 and 0 is just equal to 1
 			factorial *= i;
 		
 		return factorial;
 	}
 	
+	  public boolean find(int[] sortedValues, int value) {
+	        return search(sortedValues, value, 0, sortedValues.length - 1);
+	    }
+	 
+	    private boolean search(int[] sorted, int value, int leftIndex, int rightIndex) {
+	 
+	        // 1. index check
+	        if (leftIndex > rightIndex) {
+	            return false;
+	        }
+	 
+	        // 2. middle index
+	        int middle = (rightIndex + leftIndex) / 2;
+	 
+	        // 3. recursive invoke
+	        if (sorted[middle] > value) {
+	            return search(sorted, value, leftIndex, middle - 1);
+	        } else if (sorted[middle] < value) {
+	            return search(sorted, value, middle + 1, rightIndex);
+	        } else {
+	            return true;
+	        }
+	    }
 	// binary search in an array
-	int binarySearch(int[] a,int x){
+	public static int binarySearch(int[] a,int x){
 		int n = a.length; // get number of elements in the array
 		int lower, upper, middle; // variables for positions in the array
 		lower = 0; upper = n-1; // initialize values of lower and upper points
 		
 		while(lower<=upper){
 			middle = (lower+upper)/2;
-			if(x>a[middle]) lower = middle + 1;
-			else if(x<a[middle]) upper = middle - 1;
-			else return middle;
-		}
-		
-		return 1;
+			if (a[middle] == x){
+				return middle;
+			}
+			if (a[middle] < x){
+			   lower = middle + 1;
+			}
+			else
+				upper = middle - 1;
+		};
+		return -1;	//never found
 	}
 	
 }
